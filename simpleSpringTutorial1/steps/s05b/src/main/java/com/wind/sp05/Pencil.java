dde23f7e18c4b8def46431b6b203543b78a5abd9
jamesmuoyavus <wanda1984@yeah.net>
2021-06-11 16:37:39 +0800

    [Basic] Spring Tutorial 2018-07-02

--- simpleSpringTutorial1/steps/s05b/src/main/java/com/wind/sp05/Pencil.java
+++
@@ -1,5 +0,0 @@
-package com.wind.sp05;
-
-public interface Pencil {
-	public void use();
-}
--- simpleSpringTutorial4/steps/sw23or2/src/main/java/com/wind/web/command/ITicketCommand.java
+++
@@ -1,9 +0,0 @@
-package com.wind.web.command;
-
-import com.wind.web.dto.TicketDto;
-
-public interface ITicketCommand {
-
-	public void execute(TicketDto ticketDto);
-	
-}
