5dee101a4b8e54842cf373415542369e0f937e75
jamesmuoyavus <wanda1984@yeah.net>
2021-08-09 03:41:19 +0800

    [Basic] Spring Tutorial 2018-07-14

--- sw12a-vc-pages/src/main/java/com/wind/web/HomeController.java
+++ sw12a-vc-pages/src/main/java/com/wind/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -37,6 +38,17 @@ public class HomeController {
 		
 		System.out.println(model);
 		return "board/view";
+	}
+
+	@RequestMapping("/board/reply")
+	public ModelAndView reply() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("id", 10);
+		mv.addObject("name", "바람");
+		mv.setViewName("board/reply");
+		
+		System.out.println(mv);
+		return mv;
 	}
 	
 }
